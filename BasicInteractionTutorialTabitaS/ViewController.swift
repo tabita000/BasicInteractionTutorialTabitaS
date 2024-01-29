b58aea9dd95315214287bb5dac6f5cbf5f617631
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-01-29 12:29:42 -0600

    Initial Commit

---
+++ BasicInteractionTutorialTabitaS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicInteractionTutorialTabitaS
+//
+//  Created by Tabita Sadiq on 1/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BasicInteractionTutorialTabitaSUITests/BasicInteractionTutorialTabitaSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BasicInteractionTutorialTabitaSUITestsLaunchTests.swift
+//  BasicInteractionTutorialTabitaSUITests
+//
+//  Created by Tabita Sadiq on 1/29/24.
+//
+
+import XCTest
+
+final class BasicInteractionTutorialTabitaSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
