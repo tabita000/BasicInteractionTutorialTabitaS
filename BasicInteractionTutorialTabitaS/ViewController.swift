f63ea31cb4944e36f4eb659065ecbd1c23fe1f84
Tabita Sadiq <tfadiq@students.mchenry.edu>
2024-01-31 12:51:25 -0600

    Basic Interaction Tutorial

--- BasicInteractionTutorialTabitaS/ViewController.swift
+++ BasicInteractionTutorialTabitaS/ViewController.swift
@@ -7,10 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var labelResult: UILabel!
+
+    
+    @IBAction func TapOnButton(_ sender: Any) {
+    }
+    
+    @IBOutlet var button1: UIButton!
+    
+    @IBOutlet var button2: UIButton!
+    
+    @IBOutlet var button3: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        labelResult.text = "I just changed this!".uppercased()
+        
+        button1.layer.cornerRadius = 12
+        button2.layer.cornerRadius = 12
+        button3.layer.cornerRadius = 12
+
         // Do any additional setup after loading the view.
     }
 
